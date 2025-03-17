fa0383f7a087ea52c82c3e67aa814472d43596f1
Sergey-8057 <erste_sergey@proton.me>
2025-03-17 21:22:01 +0200

    completing task 1-10

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,24 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+// Запропонуйте користувачу ввести число за допомогою prompt().
+// Збережіть введене значення у змінну number.
+// Перевірте, чи дорівнює змінна number числу 10:
+//    Якщо так – виведіть повідомлення 'Вірно' у alert().
+//    Якщо ні – виведіть 'Невірно'.
+
+// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
+// перетворіть значення на число за допомогою Number().
+
+const number = Number(prompt('Enter number:'));
+
+function numberTen(number) {
+  if (number === 10) {
+    return alert('Вірно');
+  }
+  return alert('Невірно');
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+function numberTenTwo(number) {
+  return number === 10 ? alert('Вірно') : alert('Невірно');
+}
+
+// numberTen(number);
+numberTenTwo(number);
--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,23 @@
-function getShippingMessage(country, price, deliveryFee) {
-    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+// У змінній min лежить випадкове число від 0 до 59.
+// Визначте, в яку чверть години потрапляє
+// це число (у першу, другу, третю чи четверту).
+// Виведіть в alert відповідне повідомлення, наприклад:
+// "10 входить в першу чверть"
+
+// const min = Math.floor(Math.random() * (59 - 0) + 0);
+
+const min = Math.floor(Math.random() * (59 - 0) + 0);
+
+function findQuarter(min) {
+  if (min <= 15) {
+    return `${min} входить в першу чверть`;
+  } else if (min > 15 && min <= 30) {
+    return `${min} входить в другу чверть`;
+  } else if (min > 30 && min <= 45) {
+    return `${min} входить в третю чверть`;
+  } else {
+    return `${min} входить в четверту чверть`;
+  }
 }
 
-console.log(getShippingMessage('Australia', 120, 50));
-console.log(getShippingMessage('Germany', 80, 20));
-console.log(getShippingMessage('Sweden', 100, 20));
+console.log(findQuarter(min));
